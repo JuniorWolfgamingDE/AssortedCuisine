91469772cc89a60bf133bf7e0f9d2858833da1ca
JuniorWolfgamingDE <unconfigured@null.spigotmc.org>
2023-07-18 21:37:17 +0200

    Added Sparkling Water drinking message + 2nd recipe

--- src/main/java/net/juniorwmg/assortedcuisine/item/SparklingWaterBottleItem.java
+++ src/main/java/net/juniorwmg/assortedcuisine/item/SparklingWaterBottleItem.java
@@ -2,6 +2,7 @@
 package net.juniorwmg.assortedcuisine.item;
 
 import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.ItemStack;
@@ -10,15 +11,26 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.LivingEntity;
 
+import net.juniorwmg.assortedcuisine.procedures.SparklingWaterBottleFinishesProcedure;
+
 public class SparklingWaterBottleItem extends Item {
 	public SparklingWaterBottleItem() {
-		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(0).saturationMod(0.3f).build()));
+		super(new Item.Properties().stacksTo(16).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(0).saturationMod(0.3f).alwaysEat().build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.DRINK;
 	}
 
 	@Override
 	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
 		ItemStack retval = new ItemStack(Items.GLASS_BOTTLE);
 		super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		SparklingWaterBottleFinishesProcedure.execute(entity);
 		if (itemstack.isEmpty()) {
 			return retval;
 		} else {
---
+++ src/main/java/net/juniorwmg/assortedcuisine/procedures/SparklingWaterBottleFinishesProcedure.java
@@ -0,0 +1,14 @@
+package net.juniorwmg.assortedcuisine.procedures;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+
+public class SparklingWaterBottleFinishesProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof Player _player && !_player.level.isClientSide())
+			_player.displayClientMessage(Component.literal("\u00A77\u00A7o*You feel a subtle tickling inside your nose.*"), false);
+	}
+}
