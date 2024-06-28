6f38a574b91ea50dd8a08b2b87b1df969b76aca0
JuniorWolfgamingDE <unconfigured@null.spigotmc.org>
2024-06-28 17:33:51 +0200

    Fixed bug where Mr Cheese sound would always play after the achievement has been received, and slightly changed the "Raw Cheese" texture.

--- src/main/java/net/juniorwmg/assortedcuisine/procedures/MrCheeseCheckProcedure.java
+++ src/main/java/net/juniorwmg/assortedcuisine/procedures/MrCheeseCheckProcedure.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.BlockPos;
@@ -55,23 +54,20 @@ public class MrCheeseCheckProcedure {
 					_player.displayClientMessage(
 							Component.literal(("\u00A78\u00A7oCheese-counter: " + (entity.getCapability(AssortedcuisineModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new AssortedcuisineModVariables.PlayerVariables())).cheese_eaten)), false);
 			}
-			if ((entity.getCapability(AssortedcuisineModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new AssortedcuisineModVariables.PlayerVariables())).cheese_eaten >= 3456) {
-				if (!(entity instanceof ServerPlayer _plr4 && _plr4.level() instanceof ServerLevel
-						&& _plr4.getAdvancements().getOrStartProgress(_plr4.server.getAdvancements().getAdvancement(new ResourceLocation("assortedcuisine:mr_cheese"))).isDone())) {
-					if (entity instanceof ServerPlayer _player) {
-						Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("assortedcuisine:mr_cheese"));
-						AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
-						if (!_ap.isDone()) {
-							for (String criteria : _ap.getRemainingCriteria())
-								_player.getAdvancements().award(_adv, criteria);
-						}
+			if ((entity.getCapability(AssortedcuisineModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new AssortedcuisineModVariables.PlayerVariables())).cheese_eaten == 3456) {
+				if (entity instanceof ServerPlayer _player) {
+					Advancement _adv = _player.server.getAdvancements().getAdvancement(new ResourceLocation("assortedcuisine:mr_cheese"));
+					AdvancementProgress _ap = _player.getAdvancements().getOrStartProgress(_adv);
+					if (!_ap.isDone()) {
+						for (String criteria : _ap.getRemainingCriteria())
+							_player.getAdvancements().award(_adv, criteria);
 					}
-					if (world instanceof Level _level) {
-						if (!_level.isClientSide()) {
-							_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("assortedcuisine:mrcheese")), SoundSource.MASTER, 1, 1);
-						} else {
-							_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("assortedcuisine:mrcheese")), SoundSource.MASTER, 1, 1, false);
-						}
+				}
+				if (world instanceof Level _level) {
+					if (!_level.isClientSide()) {
+						_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("assortedcuisine:mrcheese")), SoundSource.MASTER, 1, 1);
+					} else {
+						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("assortedcuisine:mrcheese")), SoundSource.MASTER, 1, 1, false);
 					}
 				}
 			}
